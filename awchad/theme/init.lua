f14508c1e06802e6b2f8440583ebb90a8d326837
devclyde <israel.devclyde@protonmail.com>
2022-06-18 17:30:31 -0300

    feat: add client borders and gaps

--- awchad/theme/init.lua
+++ awchad/theme/init.lua
@@ -1,7 +1,18 @@
+local xrdb = require('awchad.theme.xrdb')
+
 local R = {}
 
 R.statusbar = require('awchad.theme.statusbar')
 R.workspaces = require('awchad.theme.workspaces')
 R.system_widgets = require('awchad.theme.system-widgets')
+
+R.useless_gap = xrdb.dpi(8)
+
+R.border_width = xrdb.dpi(3)
+
+R.border_color_normal = xrdb.color12
+R.border_color_active = xrdb.color04
+R.border_color_urgent = xrdb.color01
+R.border_color_new = xrdb.color10
 
 return R
